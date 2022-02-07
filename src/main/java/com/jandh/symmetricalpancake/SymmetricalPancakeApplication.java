59a9397649028bb8de83a8f8f177925e1423abce
Howard Wang <idontwannarock@gmail.com>
2022-02-07 22:32:44 +0800

    feat: init commit

---
+++ src/main/java/com/jandh/symmetricalpancake/SymmetricalPancakeApplication.java
@@ -0,0 +1,13 @@
+package com.jandh.symmetricalpancake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SymmetricalPancakeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SymmetricalPancakeApplication.class, args);
+    }
+
+}
